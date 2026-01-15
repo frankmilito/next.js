6dd62697f821b167d3546f073c655cd3989a561b
Niklas Mischkulnig <4586894+mischnic@users.noreply.github.com>
2026-01-15 20:57:11 +0100

    Turbopack: deployment_id_static_assets

--- crates/next-core/src/next_config.rs
+++ crates/next-core/src/next_config.rs
@@ -968,6 +968,9 @@ pub struct ExperimentalConfig {
     cache_components: Option<bool>,
     use_cache: Option<bool>,
     root_params: Option<bool>,
+
+    asset_deployment_id: Option<RcStr>,
+
     // ---
     // UNSUPPORTED
     // ---
@@ -992,7 +995,7 @@ pub struct ExperimentalConfig {
     fully_specified: Option<bool>,
     gzip_size: Option<bool>,
 
-    pub inline_css: Option<bool>,
+    inline_css: Option<bool>,
     instrumentation_hook: Option<bool>,
     client_trace_metadata: Option<Vec<String>>,
     large_page_data_bytes: Option<f64>,
@@ -1766,7 +1769,13 @@ impl NextConfig {
     pub async fn chunk_suffix_path(self: Vc<Self>) -> Result<Vc<Option<RcStr>>> {
         let this = self.await?;
 
-        match &this.deployment_id {
+        let deployment_id = this
+            .experimental
+            .asset_deployment_id
+            .as_ref()
+            .or(this.deployment_id.as_ref());
+
+        match deployment_id {
             Some(deployment_id) => Ok(Vc::cell(Some(format!("?dpl={deployment_id}").into()))),
             None => Ok(Vc::cell(None)),
         }
