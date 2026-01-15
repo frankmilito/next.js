953a5be3fc856b097edd601a1b40a741152660f5
Niklas Mischkulnig <4586894+mischnic@users.noreply.github.com>
2026-01-15 21:28:23 +0100

    add test

--- test/production/deployment-id-handling/app/next.config.js
+++ test/production/deployment-id-handling/app/next.config.js
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
-  deploymentId: process.env.CUSTOM_DEPLOYMENT_ID,
+  deploymentId:
+    process.env.ASSET_DEPLOYMENT_ID ?? process.env.CUSTOM_DEPLOYMENT_ID,
   experimental: {
     useSkewCookie: Boolean(process.env.COOKIE_SKEW),
+    assetDeploymentId: process.env.ASSET_DEPLOYMENT_ID
+      ? `static-${process.env.ASSET_DEPLOYMENT_ID}`
+      : undefined,
   },
 }
